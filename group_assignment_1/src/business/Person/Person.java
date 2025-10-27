e1637b1e097c4fd22368856fa4ff19f1818510ec
Niharika-Neu <thippeswamy.n@northeastern.edu>
2025-10-26 23:27:08 -0400

    admin

---
+++ group_assignment_1/src/business/Person/Person.java
@@ -0,0 +1,59 @@
+package business.Person;
+
+/**
+ * 
+ * 
+ * @author krishi
+ */
+public class Person {
+
+    private static int counter = 1;
+    private String personId;
+    private String name;
+
+    public Person(String name) {
+        this.personId = "P-" + counter++;
+        this.name = name;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + personId + ")";
+    }
+
+    public String getPersonName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getEmail() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getPhone() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPersonName(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setEmail(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPhone(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
