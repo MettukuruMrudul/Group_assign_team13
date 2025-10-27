ae953dc1089fbb1597a668fd439c33b2f5ed9503
krishi <parekh.krishi@northeastern.edu>
2025-10-26 21:04:39 -0400

    created Role.java

---
+++ group_assignment_1/src/business/UserAccounts/Role.java
@@ -0,0 +1,13 @@
+package business.UserAccounts;
+
+/**
+ * Enum for Role-based Access Control.
+ * Defines roles used in Digital University System.
+ * @author Manav
+ */
+public enum Role {
+    ADMIN,
+    FACULTY,
+    STUDENT,
+    REGISTRAR
+}
