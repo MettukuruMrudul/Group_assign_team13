59c7cec194614a7a2ad7f75a5abd4d18b7351ce4
MettukuruMrudul <mettukuru.m@northeastern.edu>
2025-10-26 22:54:00 -0400

    Student

---
+++ group_assignment_1/src/userinterface/student/StudentMain.java
@@ -0,0 +1,18 @@
+
+
+
+package userinterface.student;
+import javax.swing.*;
+import userinterface.student.StudentWorkAreaJPanel;
+public class StudentMain {
+  public static void main(String[] args){
+    SwingUtilities.invokeLater(()->{
+      JFrame f=new JFrame("University Student - Dashboard");
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      f.setSize(960,640);
+      f.setLocationRelativeTo(null);
+      f.setContentPane(new StudentWorkAreaJPanel("S001"));
+      f.setVisible(true);
+    });
+  }
+}
